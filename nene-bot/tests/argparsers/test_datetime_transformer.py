4b8bd413ca05d55d0be518fb9de8e56a4673207e
T <thomasng5000@gmail.com>
2026-08-19 13:09:26 +0000

    Fix db tz issue (#4)
    Reviewed-on: https://git.yippies.me/T/Nene-v2/pulls/4

--- nene-bot/tests/argparsers/test_datetime_transformer.py
+++ nene-bot/tests/argparsers/test_datetime_transformer.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 import discord
 import pytest
 
-from argparsers.DateTimeTransformer import SYDNEY_TZ, DateTimeTransformer
+from argparsers.DateTimeTransformer import DateTimeTransformer
 
 
 @pytest.mark.parametrize(
@@ -12,11 +12,11 @@ from argparsers.DateTimeTransformer import SYDNEY_TZ, DateTimeTransformer
     [
         (
             "03-03-2026",
-            datetime(2026, 3, 3, tzinfo=SYDNEY_TZ),
+            datetime(2026, 3, 3),
         ),
         (
             "03-03-2026 14:30",
-            datetime(2026, 3, 3, 14, 30, tzinfo=SYDNEY_TZ),
+            datetime(2026, 3, 3, 14, 30),
         ),
     ],
 )
